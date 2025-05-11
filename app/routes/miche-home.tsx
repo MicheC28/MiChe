fa8d8ab46cbde489bacbe2fd22e1e871aef3609a
miche <michelleskater28@gmail.com>
2025-05-11 16:26:41 -0400

    remove unused browerrouter

--- app/routes/miche-home.tsx
+++ app/routes/miche-home.tsx
@@ -3,7 +3,6 @@ import { Welcome } from "../welcome/welcome";
 import HomePage from "../HomePage/HomePage";
 import NavBar from "~/NavBar/NavBar";
 import { FlickeringGrid } from "components/magicui/flickering-grid";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectHomePage from "~/ProjectsPages/ProjectHomePage/ProjectHomePage";
 import MiCheHomePage from "~/MiChePages/MiCheHomePage/MiCheHomePage";
 
